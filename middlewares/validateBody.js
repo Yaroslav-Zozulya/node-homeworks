e4d6850826e0e353d44be3b0c3b846a17583c972
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-09-07 17:37:44 +0300

    add empty body check

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,6 +2,11 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const fn = (req, res, next) => {
+    const isBodyEmpty = Object.keys(req.body).length === 0;
+    if (isBodyEmpty) {
+      return next(HttpError(400, "missing fields"));
+    }
+
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
