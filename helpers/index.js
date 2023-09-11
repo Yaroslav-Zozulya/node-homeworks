4720bce7aebabddd3af00e6d6b26b502ab056044
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-09-11 19:58:11 +0300

    delete useless code

--- helpers/index.js
+++ helpers/index.js
@@ -1,11 +1,9 @@
 const { HttpError } = require("./HttpError");
 const { updateContacts } = require("./updateContatcs");
 const { ctrlWrapper } = require("./ctrlWrapper");
-const { HttpResponse } = require("./HttpResponse");
 
 module.exports = {
   HttpError,
   updateContacts,
   ctrlWrapper,
-  HttpResponse,
 };
